b7ecf49bc37272adaf0ffe19608575ef714203b6
steviewarbs <steviewarbs1@hotmail.co.uk>
2021-05-31 13:29:29 +0100

    added favicon and tidied filed

--- src/App.js
+++ src/App.js
@@ -1,53 +1,20 @@
 import "./App.css";
-import Green from '../src/green_bin.png';
-import Black from '../src/black_bin.png';
-
+import Bins from "./Bins";
 
 function App() {
-
   var date = new Date();
-
-    const bins = [
-    { date: "Monday 31st May 2021", type: "general waste + recycling", img: Black },
-    { date: "Monday 7th June 2021", type: "garden waste + recycling", img: Green },
-    { date: "Monday 14th June 2021", type: "general waste + recycling", img: Black },
-    { date: "Monday 21st June 2021", type: "garden waste + recycling", img: Green },
-    { date: "Monday 28th June 2021", type: "general waste + recycling", img: Black },
-    { date: "Monday 5th July 2021", type: "garden waste + recycling", img: Green },
-    { date: "Monday 12th July 2021", type: "general waste + recycling", img: Black },
-    { date: "Monday 19th July 2021", type: "garden waste + recycling", img: Green },
-    { date: "Monday 26th July 2021", type: "general waste + recycling", img: Black },
-    { date: "Monday 2nd August 2021", type: "garden waste + recycling", img: Green },
-    { date: "Monday 9th August 2021", type: "general waste + recycling", img: Black },
-    { date: "Monday 16th August 2021", type: "garden waste + recycling", img: Green },
-    { date: "Monday 23rd August 2021", type: "general waste + recycling", img: Black },
-    { date: "Monday 30th August 2021", type: "garden waste + recycling", img: Green },
-    { date: "Monday 6th September 2021", type: "general waste + recycling", img: Black },
-    { date: "Monday 13th September 2021", type: "garden waste + recycling", img: Green },
-    { date: "Monday 20rd September 2021", type: "general waste + recycling", img: Black },
-    { date: "Monday 27th September 2021", type: "garden waste + recycling", img: Green },
-    { date: "Monday 4th October 2021", type: "general waste + recycling", img: Black },
-    { date: "Monday 11th October 2021", type: "garden waste + recycling", img: Green },
-    { date: "Monday 18th October 2021", type: "general waste + recycling", img: Black },
-    { date: "Monday 25th October 2021", type: "garden waste + recycling", img: Green },
-    { date: "Monday 1st November 2021", type: "general waste + recycling", img: Black },
-    { date: "Monday 8th November 2021", type: "garden waste + recycling", img: Green },
-    { date: "Monday 15th November 2021", type: "general waste + recycling", img: Black },
-    { date: "Monday 22nd November 2021", type: "garden waste + recycling", img: Green },
-    { date: "Monday 29th November 2021", type: "general waste + recycling", img: Black }    
-  ];
 
   return (
     <div>
       <h1>Bindicator</h1>
-      <h3>{"Todays date " + new Date(date).toString()}</h3>
-      <h4>Rosewood Drive 2021 May-December Bindicator...</h4>
-      {bins.map((val) => {
+      {/* <h3>{"Todays date: " + new Date(date).toString()}</h3> */}
+      <h2>Mike 'n' Warbs - 2021 May-December Bindicator...</h2>
+      {Bins.map((val) => {
         return (
           <div>
-            <h2>
+            <h3>
               {val.date} {val.type}{" "}
-            </h2>{" "}
+            </h3>{" "}
             <img src={val.img} alt="bin_type"></img>;
           </div>
         );
@@ -57,5 +24,3 @@ function App() {
 }
 
 export default App;
-
-
---
+++ src/Bins.js
@@ -0,0 +1,36 @@
+
+import Green from '../src/green_bin.png';
+import Black from '../src/black_bin.png';
+
+const bins = [
+    { date: "Monday 31st May 2021", type: "general waste + recycling", img: Black },
+    { date: "Monday 7th June 2021", type: "garden waste + recycling", img: Green },
+    { date: "Monday 14th June 2021", type: "general waste + recycling", img: Black },
+    { date: "Monday 21st June 2021", type: "garden waste + recycling", img: Green },
+    { date: "Monday 28th June 2021", type: "general waste + recycling", img: Black },
+    { date: "Monday 5th July 2021", type: "garden waste + recycling", img: Green },
+    { date: "Monday 12th July 2021", type: "general waste + recycling", img: Black },
+    { date: "Monday 19th July 2021", type: "garden waste + recycling", img: Green },
+    { date: "Monday 26th July 2021", type: "general waste + recycling", img: Black },
+    { date: "Monday 2nd August 2021", type: "garden waste + recycling", img: Green },
+    { date: "Monday 9th August 2021", type: "general waste + recycling", img: Black },
+    { date: "Monday 16th August 2021", type: "garden waste + recycling", img: Green },
+    { date: "Monday 23rd August 2021", type: "general waste + recycling", img: Black },
+    { date: "Monday 30th August 2021", type: "garden waste + recycling", img: Green },
+    { date: "Monday 6th September 2021", type: "general waste + recycling", img: Black },
+    { date: "Monday 13th September 2021", type: "garden waste + recycling", img: Green },
+    { date: "Monday 20rd September 2021", type: "general waste + recycling", img: Black },
+    { date: "Monday 27th September 2021", type: "garden waste + recycling", img: Green },
+    { date: "Monday 4th October 2021", type: "general waste + recycling", img: Black },
+    { date: "Monday 11th October 2021", type: "garden waste + recycling", img: Green },
+    { date: "Monday 18th October 2021", type: "general waste + recycling", img: Black },
+    { date: "Monday 25th October 2021", type: "garden waste + recycling", img: Green },
+    { date: "Monday 1st November 2021", type: "general waste + recycling", img: Black },
+    { date: "Monday 8th November 2021", type: "garden waste + recycling", img: Green },
+    { date: "Monday 15th November 2021", type: "general waste + recycling", img: Black },
+    { date: "Monday 22nd November 2021", type: "garden waste + recycling", img: Green },
+    { date: "Monday 29th November 2021", type: "general waste + recycling", img: Black }    
+  ];
+
+
+export default bins
